89b9a5380f93b380d1a1b3f79ef19a612cd01203
Patrick Trantelle <patrick.trantelle@gmail.com>
2024-02-19 23:15:46 +0100

    remap for fast quit and save

--- lua/confusopoly/remap.lua
+++ lua/confusopoly/remap.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 
+
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
