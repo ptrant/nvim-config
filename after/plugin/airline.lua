ec8ef52aadd2e08671d80a8ac67e875453a7fc8a
Patrick Trantelle <patrick.trantelle@gmail.com>
2024-02-20 22:54:04 +0100

    airline with tabs

--- after/plugin/airline.lua
+++ after/plugin/airline.lua
@@ -1,5 +1,3 @@
 vim.g.airline_theme = "badwolf"
--- no idea how to set these up correctly:
--- vim.cmd("let g:airline#extensions#tabline#enabled = 1")
--- vim.g.airline_powerline_fonts = "1"
+vim.cmd("let g:airline#extensions#tabline#enabled = 1")
 
